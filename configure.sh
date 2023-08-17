931bbe94b92b20b89bd37b285698d94568a51bf7
ch1rish <138563037+ch1rish@users.noreply.github.com>
2023-08-16 22:11:51 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
-extracflags="-O3 -ffinite-loops -ffast-math -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
+extracflags="-O2 -ffinite-loops -ffast-math -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
 ./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=native -mtune=cortex-a55" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=native -mtune=cortex-a55  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
 
