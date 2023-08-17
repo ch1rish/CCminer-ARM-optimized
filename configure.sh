55373b12bd5dc9d4675b7bf302bd65fed5f28c94
ch1rish <138563037+ch1rish@users.noreply.github.com>
2023-08-17 12:05:11 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,4 +2,4 @@
 
 extracflags="-O3 -ffinite-loops -ffast-math -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8.2-a+crypto -mcpu=cortex-a76" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8.2-a+crypto -mcpu=cortex-a76  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
+./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mcpu=cortex-a55" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mcpu=cortex-a55  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
