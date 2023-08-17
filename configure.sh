b13e5e2c7e09ef0b750e6c57f6db872c6ff140ca
ch1rish <138563037+ch1rish@users.noreply.github.com>
2023-08-16 22:07:11 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@
 
 extracflags="-O3 -ffinite-loops -ffast-math -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -mcpu=cortex-a76 -mtune=cortex-a55" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -mcpu=cortex-a76 -mtune=cortex-a55  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
+./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=native -mtune=cortex-a55" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=native -mtune=cortex-a55  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
 
