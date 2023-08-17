099843f60c9f074746a4d8d85c40442ff22dbcff
ch1rish <138563037+ch1rish@users.noreply.github.com>
2023-08-17 10:04:28 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
-extracflags="-O3"
+extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
 
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -mcpu=cortex-a55 -mtune=cortex-a55" CFLAGS="-mcpu=cortex-a55 -mtune=cortex-a55  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
+./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8.2-a+crypto -mtune=cortex-a55" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8.2-a+crypto -mtune=cortex-a55 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize $extracflags -mllvm -enable-loop-distribute" CXX=clang++ CC=clang LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
